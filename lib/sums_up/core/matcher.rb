58a9c3784bca658037beb7743cc07b982990dddd
Tom Hulihan <hulihan.tom159@gmail.com>
2020-11-02 10:11:36 -0500

    Use modules for generated methods; set those modules as constants

--- lib/sums_up/core/matcher.rb
+++ lib/sums_up/core/matcher.rb
@@ -4,59 +4,65 @@ module SumsUp
   module Core
     # Matching DSL for sum type variants. Methods in this class are prefixed
     # with an _ so as not to conflict with the names of user-defined variant
-    # names.
+    # names. Use .build_matcher_class to generate a new subclass for a variant
+    # given the other variant names.
     class Matcher
       def self.build_matcher_class(variant, other_variants)
-        Class.new(self).tap do |klass|
-          klass.define_method(variant, &build_correct_matcher(variant))
+        mods = {
+          CorrectMatcher: correct_matcher_module(variant),
+          IncorrectMatcher: incorrect_matcher_module(other_variants),
+          FetchResult: fetch_result_module([variant, *other_variants])
+        }
 
-          other_variants.each do |other_variant|
-            klass.define_method(other_variant, &build_incorrect_matcher(other_variant))
+        mods.each_with_object(Class.new(self)) do |(const, mod), klass|
+          klass.const_set(const, mod)
+
+          klass.include(mod)
+        end
+      end
+
+      def self.correct_matcher_module(variant)
+        Module.new.tap do |mod|
+          mod.define_method(variant) do |value = nil, &block|
+            _ensure_wildcard_not_matched!(variant)
+            _ensure_no_duplicate_match!(variant)
+
+            @matched_variants << variant
+            @matched = true
+
+            @result = block ? block.call(*@variant_instance.members) : value
+
+            self
           end
-
-          all_variants = [variant, *other_variants]
-
-          klass.define_method(:_fetch_result, &build_fetch_result(all_variants))
         end
       end
 
-      def self.build_correct_matcher(variant)
-        proc do |value = nil, &block|
-          _ensure_wildcard_not_matched!(variant)
-          _ensure_no_duplicate_match!(variant)
+      def self.incorrect_matcher_module(variants)
+        variants.each_with_object(Module.new) do |variant, mod|
+          mod.define_method(variant) do |_value = nil|
+            _ensure_wildcard_not_matched!(variant)
+            _ensure_no_duplicate_match!(variant)
 
-          @matched_variants << variant
-          @matched = true
+            @matched_variants << variant
 
-          @result = block ? block.call(*@variant_instance.members) : value
-
-          self
+            self
+          end
         end
       end
 
-      def self.build_incorrect_matcher(variant)
-        proc do |_value = nil|
-          _ensure_wildcard_not_matched!(variant)
-          _ensure_no_duplicate_match!(variant)
+      def self.fetch_result_module(all_variants)
+        Module.new.tap do |mod|
+          mod.define_method(:_fetch_result) do
+            return @result if @wildcard_matched
+            return @result if @matched_variants.length == all_variants.length
 
-          @matched_variants << variant
+            unmatched_variants = (all_variants - @matched_variants).join(', ')
 
-          self
-        end
-      end
-
-      def self.build_fetch_result(all_variants)
-        total_variants = all_variants.length
-
-        proc do
-          return @result if @wildcard_matched || (@matched_variants.length == total_variants)
-
-          unmatched_variants = (all_variants - @matched_variants).join(', ')
-
-          raise(
-            UnmatchedVariantError,
-            "Did not match the following variants: #{unmatched_variants}"
-          )
+            raise(
+              UnmatchedVariantError,
+              "Did not match the following variants: #{unmatched_variants}"
+            )
+          end
         end
       end
 
@@ -74,7 +80,7 @@ module SumsUp
 
         @wildcard_matched = true
 
-        return if @matched
+        return self if @matched
 
         @result = block_given? ? yield(@variant_instance) : value
 
